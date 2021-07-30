832fcb683d96cf55091ee6d115887af3b1ae600e
earthcorp <narendra.s@earthcorp.in>
2021-07-30 21:35:22 +0530

    update upto 30.07.2021

---
+++ bs-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    proxy: "localhost:8000",
+    files: ["**/*.css", "**/*.pug", "**/*.js"],
+    ignore: ["node_modules"],
+    reloadDelay: 10,
+    ui: false,
+    notify: true,
+    port: 3001,
+};
