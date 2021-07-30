4a8471a5c141a6c90d30c6a8dd062835555abab2
Narendra Singh <33634750+earthcorp@users.noreply.github.com>
2021-07-30 22:11:21 +0530

    Delete bs-config.js

--- bs-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    proxy: "localhost:8000",
-    files: ["**/*.css", "**/*.pug", "**/*.js"],
-    ignore: ["node_modules"],
-    reloadDelay: 10,
-    ui: false,
-    notify: true,
-    port: 3001,
-};
